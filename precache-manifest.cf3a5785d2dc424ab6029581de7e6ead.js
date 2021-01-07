4905b81d7bafa2efff5865019242841b56aa7dc5
vsergeev <ssf.svaleriy@gmail.com>
2021-01-07 23:31:38 +0100

    Updates

---
+++ precache-manifest.cf3a5785d2dc424ab6029581de7e6ead.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739e781a13bf18cb794f6c9fcd4fce26",
+    "url": "/uzbegim-app/index.html"
+  },
+  {
+    "revision": "5a6af7c242751b4de406",
+    "url": "/uzbegim-app/static/css/main.8b3b58aa.chunk.css"
+  },
+  {
+    "revision": "d45ad1fd932be578fe59",
+    "url": "/uzbegim-app/static/js/2.e57336f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uzbegim-app/static/js/2.e57336f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6af7c242751b4de406",
+    "url": "/uzbegim-app/static/js/main.4dc27032.chunk.js"
+  },
+  {
+    "revision": "cdc82ac4cb2fccc0ee80",
+    "url": "/uzbegim-app/static/js/runtime-main.c41d731b.js"
+  },
+  {
+    "revision": "78f6313eeaab8de960bb285666f3b7bb",
+    "url": "/uzbegim-app/static/media/bg_1.78f6313e.jpg"
+  },
+  {
+    "revision": "92ec4fe802faac2886f9dcb29ee0e0e0",
+    "url": "/uzbegim-app/static/media/dessert-5.92ec4fe8.jpg"
+  },
+  {
+    "revision": "d3cf882213dc37a559cb0d7a05bdf426",
+    "url": "/uzbegim-app/static/media/restaurant.d3cf8822.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/uzbegim_app/precache-manifest.0c68cb7438b214479a51c058cca579d7.js"
+  "/uzbegim-app/precache-manifest.cf3a5785d2dc424ab6029581de7e6ead.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uzbegim_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uzbegim-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
