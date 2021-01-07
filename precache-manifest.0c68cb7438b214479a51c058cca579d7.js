a7cbc02ddb6629499ece422aea912b418a84b133
vsergeev <ssf.svaleriy@gmail.com>
2021-01-07 23:28:06 +0100

    Updates

---
+++ precache-manifest.0c68cb7438b214479a51c058cca579d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cbb6064941f71b51c27bf266cd46d67",
+    "url": "/uzbegim_app/index.html"
+  },
+  {
+    "revision": "bfc83f5150eba23902e0",
+    "url": "/uzbegim_app/static/css/main.8b3b58aa.chunk.css"
+  },
+  {
+    "revision": "d45ad1fd932be578fe59",
+    "url": "/uzbegim_app/static/js/2.e57336f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uzbegim_app/static/js/2.e57336f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc83f5150eba23902e0",
+    "url": "/uzbegim_app/static/js/main.60ae8414.chunk.js"
+  },
+  {
+    "revision": "efb24d75cd4aa3f32d86",
+    "url": "/uzbegim_app/static/js/runtime-main.cdc70caa.js"
+  },
+  {
+    "revision": "78f6313eeaab8de960bb285666f3b7bb",
+    "url": "/uzbegim_app/static/media/bg_1.78f6313e.jpg"
+  },
+  {
+    "revision": "92ec4fe802faac2886f9dcb29ee0e0e0",
+    "url": "/uzbegim_app/static/media/dessert-5.92ec4fe8.jpg"
+  },
+  {
+    "revision": "d3cf882213dc37a559cb0d7a05bdf426",
+    "url": "/uzbegim_app/static/media/restaurant.d3cf8822.jpg"
+  }
+]);
