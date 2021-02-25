4bf919e306846001cafb109ff25e973349e532f3
vsergeev <ssf.svaleriy@gmail.com>
2021-02-25 01:06:07 +0100

    Updates

---
+++ precache-manifest.88b7337756baca898f23c2fd102e1c53.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f2219c0a94497ebe3459ad73401899",
+    "url": "/uzbegim-app/index.html"
+  },
+  {
+    "revision": "75a875989814204b66b1",
+    "url": "/uzbegim-app/static/css/main.c716636d.chunk.css"
+  },
+  {
+    "revision": "dd5f6eebf9c36ed87d98",
+    "url": "/uzbegim-app/static/js/2.5b555638.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uzbegim-app/static/js/2.5b555638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a875989814204b66b1",
+    "url": "/uzbegim-app/static/js/main.c39677a9.chunk.js"
+  },
+  {
+    "revision": "cdc82ac4cb2fccc0ee80",
+    "url": "/uzbegim-app/static/js/runtime-main.c41d731b.js"
+  },
+  {
+    "revision": "d804fab41769a4bc204e7c3d46787448",
+    "url": "/uzbegim-app/static/media/bg_1.d804fab4.jpg"
+  },
+  {
+    "revision": "296ee5127b22dc595adb033287b37411",
+    "url": "/uzbegim-app/static/media/city-map-bw.296ee512.png"
+  },
+  {
+    "revision": "92ec4fe802faac2886f9dcb29ee0e0e0",
+    "url": "/uzbegim-app/static/media/dessert-5.92ec4fe8.jpg"
+  },
+  {
+    "revision": "d3cf882213dc37a559cb0d7a05bdf426",
+    "url": "/uzbegim-app/static/media/restaurant.d3cf8822.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.671bbbbc15e1158fca5a9bf21f682245.js"
+  "/uzbegim-app/precache-manifest.88b7337756baca898f23c2fd102e1c53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uzbegim-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
