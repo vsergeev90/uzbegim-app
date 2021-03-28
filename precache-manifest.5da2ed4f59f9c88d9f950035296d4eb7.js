48dc6d29bd415db06d16e6142faec79f7323efcd
vsergeev <ssf.svaleriy@gmail.com>
2021-03-29 01:00:09 +0200

    Updates

---
+++ precache-manifest.5da2ed4f59f9c88d9f950035296d4eb7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f839dae0dfd202a5435c6663c3590d",
+    "url": "/uzbegim/index.html"
+  },
+  {
+    "revision": "4356c9063e48e9cbd7fc",
+    "url": "/uzbegim/static/css/main.e156480f.chunk.css"
+  },
+  {
+    "revision": "917b3247bfdefb0dc0b2",
+    "url": "/uzbegim/static/js/2.94d9ca0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/uzbegim/static/js/2.94d9ca0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4356c9063e48e9cbd7fc",
+    "url": "/uzbegim/static/js/main.55de8b37.chunk.js"
+  },
+  {
+    "revision": "b813ad1c6bc5e0a28a2c",
+    "url": "/uzbegim/static/js/runtime-main.5a527b43.js"
+  },
+  {
+    "revision": "2339d267f9559b2492ad9c61196129a6",
+    "url": "/uzbegim/static/media/bg_1.2339d267.jpg"
+  },
+  {
+    "revision": "296ee5127b22dc595adb033287b37411",
+    "url": "/uzbegim/static/media/city-map-bw.296ee512.png"
+  },
+  {
+    "revision": "d3cf882213dc37a559cb0d7a05bdf426",
+    "url": "/uzbegim/static/media/restaurant.d3cf8822.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b66176f141570ff8027c812ccdc31a94.js"
+  "/uzbegim/precache-manifest.5da2ed4f59f9c88d9f950035296d4eb7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uzbegim/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
